808e35de160eaf0e7515abd7d0a5677d142d7dcc
mohamedelraghy <elraghy8@gmail.com>
2021-09-13 14:52:44 +0200

    shortcuts for common data types in contest

--- c++.cpp
+++ c++.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 #define P(x) cout << #x << " = { " << x << " }\n"
+#define INF 1000000000  
+
+// shortcuts for "common" data types in contests
+typedef long long       ll;
+typedef pair<int, int>  ii;
+typedef vector<ii>      vii;
+typedef vector<int>     vi;
+
+// common memset setting 
+// memset(memo, -1, sizeof memo); // initialize DP memoization table with -1
+// memset(arr, 0, sizeof arr);    // to clear array of integers 
 
 
 int main(){
